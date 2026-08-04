dfc7cdababb03742f2d21bfecf53df621116d316
yurymenichelli-ai <yurymenichelli@gmail.com>
2026-08-04 17:39:18 +0200

    Aggiorna cache riepiloghi finanziari

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iki-studio-v4';
+const CACHE='iki-studio-v5';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/icons/iki-studio-logo.png']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
